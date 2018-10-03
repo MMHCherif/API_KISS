144261ec187cf10bd0cb9612626d62ed98d90395
mcherif <mmmcherif@hotmail.com>
2018-10-03 04:40:26 +0200

    CTUD category et brand fpour l'Api

---
+++ src/Controller/API/BrandControler.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Controller\API;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use App\Entity\Brand;
+use FOS\RestBundle\View\View ;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use JMS\Serializer;
+use App\Repository\ProductRepository;
+class BrandController extends FOSRestController 
+{
+	 /**
+     * Creates an Product resource
+     * @Rest\Post("/Brand")
+     * 
+     * @param Request $request
+     * @return View
+     */
+    public function postProduct(Request $request): View
+    {
+       	$ = new Brand();
+        $Brand->setName($request->get('name'));
+        $Brand->setId(md5($request->get('id')));
+        $em = $this->getDoctrine()->getManager();
+        $em	->add($Brand);
+           	->flush();
+        $this->serialize($Brand,'JSON')  ; 
+        return View::create($Brand, Response::HTTP_OK);
+    }
+     /**
+     * Retrieves an Brand resource
+     * @Rest\Get("/Brand/{id}")
+     */
+   
+    public function getBrand($id): View
+    {
+        $Brand = $this->getDoctrine()
+        				->getRepository(Brand::class)
+        				->find($id)
+        				;
+        $this->serialize($Brand,'JSON')  ; 
+        return View::create($Brand, Response::HTTP_OK);
+    }
+    /**
+     * Delete an Brand resource
+     * @Rest\Delete("/Brand/{id}")
+     * @param IdBrand
+     * @return View
+     */
+    public function DeleteBrand($id): View
+    {
+    	if ($this->isCsrfTokenValid('delete'.$Brand->getId(), $request->request->get('_token'))) 
+    	{
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($Brand);
+            $em->flush();
+    	}
+    	$res=serialize($Brand);
+    	return View::create($res, Response::HTTP_OK);
+    }
+    /**
+     * Delete an Brand resource
+     * @Rest\Put("/Brand/{id}")
+     * @param Request $request
+     * @return View
+     */
+    public function PutBrand(request $request): View
+    {
+    	$Brand=$this->deserialize($request->getData());
+    		
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($Brand);
+        $em->flush();
+    	
+    	$res=serialize($Brand);
+    	return View::create($res, Response::HTTP_OK);
+    }
+
+}
---
+++ src/Controller/API/CategoryController.php
@@ -0,0 +1,81 @@
+<?php
+namespace App\Controller\API;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use App\Entity\Category;
+use FOS\RestBundle\View\View ;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use JMS\Serializer;
+use App\Repository\ProductRepository;
+class CategoryController extends FOSRestController 
+{
+	 /**
+     * Creates an Product resource
+     * @Rest\Post("/category")
+     * 
+     * @param Request $request
+     * @return View
+     */
+    public function postProduct(Request $request): View
+    {
+       	$ = new Category();
+        $Category->setName($request->get('name'));
+        $Category->setId(md5($request->get('id')));
+        $em = $this->getDoctrine()->getManager();
+        $em	->add($Category);
+           	->flush();
+        $this->serialize($Category,'JSON')  ; 
+        return View::create($Category, Response::HTTP_OK);
+    }
+     /**
+     * Retrieves an Category resource
+     * @Rest\Get("/Category/{id}")
+     */
+   
+    public function getCategory($id): View
+    {
+        $Category = $this->getDoctrine()
+        				->getRepository(Category::class)
+        				->find($id)
+        				;
+        $this->serialize($Category,'JSON')  ; 
+        return View::create($Category, Response::HTTP_OK);
+    }
+    /**
+     * Delete an Category resource
+     * @Rest\Delete("/Category/{id}")
+     * @param IdCategory
+     * @return View
+     */
+    public function DeleteCategory($id): View
+    {
+    	if ($this->isCsrfTokenValid('delete'.$Category->getId(), $request->request->get('_token'))) 
+    	{
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($Category);
+            $em->flush();
+    	}
+    	$res=serialize($Category);
+    	return View::create($res, Response::HTTP_OK);
+    }
+    /**
+     * Delete an Category resource
+     * @Rest\Put("/Category/{id}")
+     * @param Request $request
+     * @return View
+     */
+    public function PutCategory(request $request): View
+    {
+    	$Category=$this->deserialize($request->getData());
+    		
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($Category);
+        $em->flush();
+    	
+    	$res=serialize($Category);
+    	return View::create($res, Response::HTTP_OK);
+    }
+
+}
